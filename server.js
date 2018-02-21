3955a22bdce626c521688b4d2e29d892b49fbfa9
tirth2412 <36692055+tirth2412@users.noreply.github.com>
2018-02-21 23:14:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res) {
-    var articleName=req.param.articleName;
+    var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 
