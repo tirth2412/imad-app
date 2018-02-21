16200c7df895fb8ab7f5592c97de28395493a294
tirth2412 <36692055+tirth2412@users.noreply.github.com>
2018-02-21 22:42:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,56 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var content=
+{
+    title:"Article-1",
+    heading:"Article one",
+    content:`<p>
+                    This is for testing purpose only.
+                    All you can Understand...:)
+                </p>
+                <p>
+                    This is for testing purpose only.
+                    All you can Understand...:)
+                </p>
+                <p>
+                    This is for testing purpose only.
+                    All you can Understand...:)
+                </p>`
+};
+
+function createTemplate(data)
+{
+    var title=data.title;
+    var heading=data.heading;
+    var content=data.content;
+    var htmlTemplate=
+    `<html>
+    <head>
+        <title>Article-3</title>
+        <meta name="viewport" content="width, initial-scale=1">
+        <link href="ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            <div>
+                <a href="/">HOME</a>
+            </div>
+            <hr>
+            <h3>
+                $[heading]
+            </h3>
+            <div>
+                $[content]
+            </div>
+        </div>
+    </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
