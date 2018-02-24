d98317b4c6feb6f63180d6ab5d6a6e7faeef0ad6
tirth2412 <36692055+tirth2412@users.noreply.github.com>
2018-02-24 23:50:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,8 +109,8 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var counter=0;
-app.get('/counter1',function (req,res) {
+var counter=1;
+app.get('/counter',function (req,res) {
   counter=counter+1;
   res.send(counter.toString());
 });
