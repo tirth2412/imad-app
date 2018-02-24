da72c63c660908409fecbe55679bd20ba80061ae
tirth2412 <36692055+tirth2412@users.noreply.github.com>
2018-02-24 21:41:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
 console.log('Loaded!');
+
+//changing the Text of my site
+var img=document.getElementById("img");
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+100;
+    img.style.marginLeft=marginLeft+'px';
+}
+img.onclick=function()
+{
+    var interval=setInterval(moveRight,50);
+};
