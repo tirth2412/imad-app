cab35b2af4f6164476f3c3d7539f97e05f1e5532
tirth2412 <36692055+tirth2412@users.noreply.github.com>
2018-02-25 00:03:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,6 +96,12 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+var counter=0;
+app.get('/hello',function (req,res) {
+  counter=counter+1;
+  res.send("gfdnk");
+});
+
 app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
@@ -107,12 +113,6 @@ app.get('/ui/main.js', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-var counter=0;
-app.get('/hello',function (req,res) {
-  counter=counter+1;
-  res.send("gfdnk");
 });
 
 
