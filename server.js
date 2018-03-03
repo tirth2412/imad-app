ae2ad8f5a19161a9b1fe840de54a01b5fa476952
tirth2412 <36692055+tirth2412@users.noreply.github.com>
2018-03-03 22:14:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,10 +122,10 @@ app.get('/counter',function (req,res) {
   res.send(counter.toString());
 });
 
-app.get('article/:articleName', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
   //var articleName=req.params.articleName;
   //res.send(createTemplate(articles[articleName]));
-  pool.query("SELECT * FROM article where title='" + req.params.articleName + "'",function(err,result){
+  pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'",function(err,result){
       if(err)
       { res.status(500).send(err.toString());    }
       else
