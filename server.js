49ae7d3e149675d614b0a0079cced15deae7d905
tirth2412 <36692055+tirth2412@users.noreply.github.com>
2018-03-03 20:47:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/test-db',function(req,res){
       if(err)
       { res.status(500).send(err.toString());   }
       else
-      { res.send(JSON.stringify(result));   }
+      { res.send(JSON.stringify(result.rows));   }
    });
 });
 
